5c5d4de1506adc3c0ac1aab89e3bd2053c12ba80
Benjamin Short <benjamin.short19@gmail.com>
2025-12-15 21:19:00 +1100

    luau

--- love-stub.lua
+++
@@ -1,9 +0,0 @@
----@class love
----@field graphics table
----@field window table
----@field audio table
----@field filesystem table
----@field keyboard table
----@field mouse table
-
-_G.love = _G.love or {}
--- src/prisma/components/transform.lua
+++
@@ -1,5 +0,0 @@
-return concord.component("Transform", function(self, x, y, w, h, r)
-    self.Position = batteries.vec2(x or 0, y or 0)
-    self.Size = batteries.vec2(w or 0, h or 0)
-    self.Rotation = r or 0
-end)
